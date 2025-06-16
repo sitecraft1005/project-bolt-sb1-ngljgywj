ac13fda31934dd7507da1250f5f20972d740b957
sitecraft1005 <sitecraft1005@hotmail.com>
2025-06-16 23:42:22 +0530

    Fix SVG data URL quotes in Hero component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -29,7 +29,7 @@ const Hero: React.FC = () => {
         <div className="absolute w-20 h-20 sm:w-32 sm:h-32 lg:w-48 lg:h-48 bg-gradient-to-r from-blue-400 to-cyan-500 opacity-20 rounded-full animate-float animate-morphing bottom-10 left-1/4 sm:bottom-20" style={{animationDelay: '2s'}}></div>
         
         {/* Grid pattern */}
-        <div className="absolute inset-0 bg-[url('data:image/svg+xml,%3Csvg width="60" height="60" viewBox="0 0 60 60" xmlns="http://www.w3.org/2000/svg"%3E%3Cg fill="none" fill-rule="evenodd"%3E%3Cg fill="%239C92AC" fill-opacity="0.1"%3E%3Ccircle cx="30" cy="30" r="1"/%3E%3C/g%3E%3C/g%3E%3C/svg%3E')] opacity-30"></div>
+        <div className="absolute inset-0 bg-[url('data:image/svg+xml,%3Csvg width=\"60\" height=\"60\" viewBox=\"0 0 60 60\" xmlns=\"http://www.w3.org/2000/svg\"%3E%3Cg fill=\"none\" fill-rule=\"evenodd\"%3E%3Cg fill=\"%239C92AC\" fill-opacity=\"0.1\"%3E%3Ccircle cx=\"30\" cy=\"30\" r=\"1\"/%3E%3C/g%3E%3C/g%3E%3C/svg%3E')] opacity-30"></div>
         
         {/* Glowing particles */}
         <div className="absolute top-1/4 left-1/3 w-2 h-2 bg-yellow-400 rounded-full animate-pulse opacity-60"></div>
@@ -147,7 +147,6 @@ const Hero: React.FC = () => {
         </div>
       </div>
     </section>
-  )
   );
 };
 
